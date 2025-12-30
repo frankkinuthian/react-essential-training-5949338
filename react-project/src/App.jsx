3452da973d6362bcb9be635078aab5f424686d71
Frank Kinuthia <150331312+frankkinuthian@users.noreply.github.com>
2025-12-30 02:45:20 +0000

    Refactor Main component layout and add header for today's specials

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import chef from "./images/chef.jpg";
 
 function Header({ name, year }) {
   return (
@@ -12,23 +13,31 @@ const items = [
   "Macaroni and Cheese",
   "Salmon with Potatoes",
   "Tofu with Vegetables",
-  "Minestrone Soup"
+  "Minestrone Soup",
 ];
 
 const dishObjects = items.map((dish, i) => ({
   id: i,
-  title: dish
+  title: dish,
 }));
 
 function Main({ dishes }) {
   return (
-    <ul>
-      {dishes.map((dish) => (
-        <li key={dish.id} style={{ listStyleType: "none" }}>
-          {dish.title}
-        </li>
-      ))}
-    </ul>
+    <>
+    <div>
+      <h1>Today's Specials</h1>
+    </div>
+      <main>
+        <img src={chef} alt="Chef" height={200} />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
