4ffffdd01dd5cd9026d35fa468ead148ed9330c6
Frank Kinuthia <150331312+frankkinuthian@users.noreply.github.com>
2025-12-30 02:30:14 +0000

    Refactor App component to replace main section with dynamic dish list

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -9,13 +9,29 @@ function Header({ name, year }) {
   );
 }
 
+const items = [
+  { id: 1, name: "Mac and Cheese", price: 1.2 },
+  { id: 2, name: "Salmon with potatoes", price: 0.5 },
+  { id: 3, name: "Tofu with vegetables", price: 2.0 },
+];
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li key={dish.id} style={{ listStyleType: "none" }}>
+          {dish.name} - ${dish.price.toFixed(2)}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header name="Alex" year={new Date().getFullYear()} />
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Main dishes={items} />
     </div>
   );
 }
