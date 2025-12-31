83dc0738f311eb2a8963e3ed21e559c27d9301e7
Frank Kinuthia <150331312+frankkinuthian@users.noreply.github.com>
2025-12-31 02:19:38 +0000

    Add animal array and log first animal in main.jsx

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+const animals = ["Cat", "Dog", "Elephant", "Giraffe"];
+console.log(animals[0])
+
 createRoot(document.getElementById("root")).render(<App />);
